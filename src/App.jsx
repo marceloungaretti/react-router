d6aa64b46a7124e0bd95aebdb75de5a77a449ba7
Marcelo <marcelo.ungaretti@gmail.com>
2021-10-21 00:44:00 -0300

    capitulo 2 concluido

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,23 @@ import './assets/css/base/base.css'
 import Home from './paginas/Home'
 import Sobre from './paginas/Sobre'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Pagina404 from './paginas/Pagina404.jsx'
+import Cabecalho from './components/Cabecalho'
 
 function App() {
   return (
     <>
       <Router>
+        <Cabecalho />
         <Switch>
           <Route exact path='/'>
             <Home />
           </Route>
           <Route path='/sobre'>
             <Sobre />
+          </Route>
+          <Route>
+            <Pagina404 />
           </Route>
         </Switch>
       </Router>
