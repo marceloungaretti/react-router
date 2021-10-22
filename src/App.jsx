0a08695042f1205b6085f8e97b4efa489203e32c
Marcelo <marcelo.ungaretti@gmail.com>
2021-10-22 00:48:53 -0300

    curso concluido

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Pagina404 from './paginas/Pagina404.jsx'
 import Cabecalho from './components/Cabecalho'
 import Post from './paginas/Post'
+import Categoria from './paginas/Categoria'
 
 function App() {
   return (
@@ -18,6 +19,9 @@ function App() {
           </Route>
           <Route path='/sobre'>
             <Sobre />
+          </Route>
+          <Route path='/categoria/:id'>
+            <Categoria />
           </Route>
           <Route path='/posts/:id'>
             <Post />
